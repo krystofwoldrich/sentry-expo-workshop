362f97437e66bfdbdf8a9986a4e870815b755eb1
Krystof Woldrich <krystof.woldrich@sentry.io>
2024-04-15 13:02:06 +0200

    Use Sentry SDKs DNS

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,6 @@
+
 export const DSN =
-  'https://3d19b89ce4774ad2b429b09a9375fbbb@o87286.ingest.sentry.io/5716557';
+  'https://1df17bd4e543fdb31351dee1768bb679@o447951.ingest.sentry.io/5428561';
 
 export const BACKEND_URL =
   'https://application-monitoring-flask-dot-sales-engineering-sf.appspot.com';
